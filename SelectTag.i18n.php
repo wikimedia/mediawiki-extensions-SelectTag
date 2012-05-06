2857b78dceaacef1e97bf38f9aed19bb537b4e96
raymond <raimond.spekking@gmail.com>
2012-05-06 22:28:44 +0200

    Preparations for adding extension to Translatewiki.net
    * Add i18n file
    * Fix indention of the extension credit array
    * Add path to the extension credit
    * Shorten description message to the technically relevant part
    
    Patch set 2+3: Address review comments:
    * Remove double tabs
    * Remove spaces inside []
    * Remove trailing space
    
    Change-Id: I4076e02236236c5a7bd256f6475615bb7adc541a

---
+++ SelectTag.i18n.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Internationalization file for SelectTag
+ *
+ * @file
+ * @ingroup Language
+ * @ingroup I18n
+ */
+
+$messages = array();
+
+/** English
+ * @author Khaled El Mansoury
+ */
+$messages['en'] = array(
+	'selecttag-desc' => 'Adds customisable &lt;select&gt; for inserting text via database fetching',
+);
+
--- SelectTag.php
+++ SelectTag.php
@@ -20,79 +20,78 @@ MediaWiki page: http://www.mediawiki.org/wiki/Extension:SelectTag
 */
 
 if ( !defined( 'MEDIAWIKI' ) ) {
-		die( 'This file is a MediaWiki extension, it is not a valid entry point' );
+	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
 
-$wgExtensionCredits[ 'parserhook' ][ ] = array(
-		'name' => 'SelectTag',
-		'author' => '[http://www.expressprogs.com Khaled El Mansoury]',
-		'url' => 'https://www.mediawiki.org/wiki/Extension:SelectTag',
-		'description' => 'Adds customisable &lt;select&gt; for inserting text via database fetching, perfect to be used for including quotes as well as religious texts.',
-		'version' => '1.0.1' 
+$wgExtensionCredits['parserhook'][] = array(
+	'path' => __FILE__,
+	'name' => 'SelectTag',
+	'author' => '[http://www.expressprogs.com Khaled El Mansoury]',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:SelectTag',
+	'descriptionmsg' => 'selecttag-desc',
+	'version' => '1.0.2'
 );
 
-$wgHooks[ 'ParserFirstCallInit' ][ ] = 'wfSelectTagParserInit';
+$wgHooks['ParserFirstCallInit'][] = 'wfSelectTagParserInit';
 
-function wfSelectTagParserInit( Parser $parser )
-{
-		$parser->setHook( 'select', 'wfSelectTagRender' );
-		return true;
+function wfSelectTagParserInit( Parser $parser ) {
+	$parser->setHook( 'select', 'wfSelectTagRender' );
+	return true;
 }
 
-function wfSelectTagRender( $input, array $params, Parser $parser, PPFrame $frame )
-{
-		global $wgSelectTag;
-		$parser->disableCache();
-		$dbr = wfGetDB( DB_SLAVE );
-		if ( isset( $params[ '_source' ] ) ) {
-				$sourcearray = $wgSelectTag[ $params[ '_source' ] ];
-		} else {
-				return '<div style="color: red;">Error! Source Tag not specified!</div>';
-		}
-		
-		$dbtable    = $sourcearray[ "_dbname" ];
-		$cond       = '';
-		$cond_array = array( );
-		
-		if ( isset( $params[ '_show' ] ) ) {
-				$show = $sourcearray[ "_show" ][ $params[ '_show' ] ];
-		} else {
-				$show = $sourcearray[ "_show" ][ $sourcearray[ "_showDefault" ] ];
-		}
-		array_push( $cond_array, $show );
-		
-		foreach ( $sourcearray as $key => $value ) {
-				if ( !is_array( $value ) && substr( $value, 0, 0 ) != "_" ) {
-						if ( isset( $params[ $key ] ) ) {
-								$params[ $key ] = $parser->recursiveTagParse( $params[ $key ], $frame );
-								$params[ $key ] = $dbr->addQuotes( $params[ $key ] );
-								if ( strpos( $params[ $key ], "-" ) > -1 ) {
-										$values_array = explode( "-", $params[ $key ] );
-										$cond_value   = $value . " BETWEEN " . $values_array[ 0 ] . " AND " . $values_array[ 1 ];
-										unset( $values_array );
-								} else {
-										$cond_value = $value . "=" . $params[ $key ] . "";
-								}
-								
-								if ( $cond == '' ) {
-										$cond = $cond_value;
-								} else {
-										$cond .= " AND " . $cond_value;
-								}
-								
-								array_push( $cond_array, $value );
-						}
-				}
-		}
-		
-		$res          = $dbr->select( $dbtable, $cond_array, $cond, __METHOD__, array( ) );
-		$return_value = '';
-		while ( $row = $dbr->fetchRow( $res ) ) {
-				if ( $return_value == '' ) {
-						$return_value = $row[ $show ];
-				} else {
-						$return_value .= "<br />" . $row[ $show ];
-				}
-		}
-		return $return_value;
+function wfSelectTagRender( $input, array $params, Parser $parser, PPFrame $frame ) {
+	global $wgSelectTag;
+	$parser->disableCache();
+	$dbr = wfGetDB( DB_SLAVE );
+	if ( isset( $params['_source'] ) ) {
+			$sourcearray = $wgSelectTag[$params['_source']];
+	} else {
+			return '<div style="color: red;">Error! Source Tag not specified!</div>';
+	}
+
+	$dbtable    = $sourcearray["_dbname"];
+	$cond       = '';
+	$cond_array = array( );
+
+	if ( isset( $params['_show'] ) ) {
+			$show = $sourcearray["_show"][$params['_show']];
+	} else {
+			$show = $sourcearray["_show"][$sourcearray["_showDefault"]];
+	}
+	array_push( $cond_array, $show );
+
+	foreach ( $sourcearray as $key => $value ) {
+			if ( !is_array( $value ) && substr( $value, 0, 0 ) != "_" ) {
+					if ( isset( $params[$key] ) ) {
+							$params[$key] = $parser->recursiveTagParse( $params[$key], $frame );
+							$params[$key] = $dbr->addQuotes( $params[$key] );
+							if ( strpos( $params[$key], "-" ) > -1 ) {
+									$values_array = explode( "-", $params[$key] );
+									$cond_value   = $value . " BETWEEN " . $values_array[0] . " AND " . $values_array[1];
+									unset( $values_array );
+							} else {
+									$cond_value = $value . "=" . $params[$key] . "";
+							}
+
+							if ( $cond == '' ) {
+									$cond = $cond_value;
+							} else {
+									$cond .= " AND " . $cond_value;
+							}
+
+							array_push( $cond_array, $value );
+					}
+			}
+	}
+
+	$res          = $dbr->select( $dbtable, $cond_array, $cond, __METHOD__, array( ) );
+	$return_value = '';
+	while ( $row = $dbr->fetchRow( $res ) ) {
+			if ( $return_value == '' ) {
+					$return_value = $row[$show];
+			} else {
+					$return_value .= "<br />" . $row[$show];
+			}
+	}
+	return $return_value;
 }
